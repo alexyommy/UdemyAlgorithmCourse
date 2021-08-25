bd68fd32da1c6682c7d4935d1656dbf24f9c3a1f
Alex Yom <alexyom24@gmail.com>
2021-08-24 22:07:29 -0400

    search trees

---
+++ Trees/oLogN.js
@@ -0,0 +1,11 @@
+// Level 0: 2^0 = 1 Node;
+// Level 1: 2^1 = 2 Nodes;
+// Level 2: 2^2 = 4 Nodes;
+// Level 3: 2^3 = 8 Nodes;
+
+// # of nodes = 2^h -1
+// (h=height)
+// log nodes = steps
+
+// log 100 = 2
+// 10^2 = 100;
