08da128da6cf76def36b378ac775181ae367e7e0
Alex Yom <alexyom24@gmail.com>
2021-08-25 22:54:44 -0400

    search tree

--- Trees/binarySearchTree.js
+++ Trees/binarySearchTree.js
@@ -6,19 +6,70 @@ class Node {
     }
 }
 
-class BinarSearchTree {
+class BinarySearchTree {
     constructor(){
         this.root = null;
     }
     insert(value){
-
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while(true) {
+                if(value < currentNode.value){
+                    // Left
+                    if(!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    // Right
+                    if(!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
     }
     lookup(value){
+        if(!this.root) {
+            return false;
+        }
+        let currentNode = this.root; 
+        while(currentNode) {
+            if (value < currentNode.value ) {
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                return currentNode;
+            }
+        }
+        return false;
+    }
+    remove(value){
+        if (!this.root){
+            return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while(currentNode){
+            if(value < currentNode.value){
+                
+            } else if (value > currentNode.value){
 
+            } else if (currentNode.value ===value){
+                
+            }
+        }
     }
 }
 
-const tree = new BinarSearchTree();
+const tree = new BinarySearchTree();
 tree.insert(9);
 tree.insert(4);
 tree.insert(6);
@@ -26,7 +77,8 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-JSON.stringify(traverse(tree.root))
+console.log(tree.lookup(10))
+// console.log(JSON.stringify(traverse(tree.root)))
 
 //      9
 //   4     20
@@ -34,10 +86,9 @@ JSON.stringify(traverse(tree.root))
 
 function traverse(node) {
     const tree = { value: node.value };
-        tree.left = node.left === null ? null :
-        traverse(node.left);
-        tree.right = node.right === null ? null :
-        traverse(node.right);
-        return tree;
-    }
+    tree.left = node.left === null ? null :
+    traverse(node.left);
+    tree.right = node.right === null ? null :
+    traverse(node.right);
+    return tree;
 }
